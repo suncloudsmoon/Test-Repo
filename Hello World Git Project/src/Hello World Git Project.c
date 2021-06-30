18cbb1e83c393fc6032749eb87129371ed0e5eff
suncloudsmoon <gajjam2003@gmail.com>
2021-06-30 13:58:39 -0700

    Hopefully It Works

---
+++ Hello World Git Project/src/Hello World Git Project.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Hello.c
+ Author      : suncloudsmoon
+ Version     :
+ Copyright   : Copyright (c) 2021, suncloudsmoon.
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
